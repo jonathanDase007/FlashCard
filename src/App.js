381503a1881caae1ffee733630c2ae471c1f8b57
jonathanDasee007 <jonathan.dasee@sps.edu>
2022-12-22 11:41:02 -0500

    Keys

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
+import React, {useState} from 'react';
+import { useEffect } from 'react';
+
 import FlashBody from "./flashcard/components/Flashcard.js";
 
 const questions = [
   ['servus'],
-  ['Question b is right'],
-  ['Question c is right'],
-  ['Question d is right']
+  ['slave'],
+  ['amicus'],
+  ['friend']
 ]
 
+
 const App = () => {
+  //Defines state of pressed or not
+  const [active, setActive] = useState(0);
+
+  //Defines Flashcard Iteration
+  const [count, setCount] = useState(0);
+
+  //Changing between definition or not
+  const handleClick = () => {
+    setActive(active => active + 1);
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', keyDown, true)
+  }, [])
+
+  const keyDown = (e) => {
+    console.log("Clicked key: ", e.key)
+
+  
+    if(e.key === ' '){
+      setActive(active => active + 1);
+    } else if(e.key === "ArrowRight"){
+      setCount(count + 1)
+    } else if(e.key === "ArrowLeft"){
+      setCount(count - 1)
+    }
+
+    
+  };
+
+  //Flashcard Graphic
   return (
     <FlashBody 
-      value = {questions[0]}
+      value = {active%2 === 1 ? questions[count] : questions[count + 1]}
+      onClick = {handleClick}
     /> 
   );
   };
